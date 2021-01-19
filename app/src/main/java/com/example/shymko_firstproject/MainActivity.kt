fc8fde64168c0cc952a7db44f56862f3c90113c9
ShymkoGleb <71500261+ShymkoGleb@users.noreply.github.com>
2021-01-19 16:24:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shymko_firstproject/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.shymko_firstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*Game score app:
+- app consists of 4 screens: enter 2 teams screen, score screen, winner screen, list of winners screen
+- score screen has count down timer for the game and 2 buttons for each team to increment the score
+- user can cancel the game
+- each winner should be saved to the list of winners after the game (within the app session)
+- user can review list of winners sorted by score in descending order
+- user can clear the list of winners
+- user can share results of the game*/
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
