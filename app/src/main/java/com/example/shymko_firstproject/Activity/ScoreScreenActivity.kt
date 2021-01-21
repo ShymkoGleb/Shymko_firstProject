06a64bda578cf88166431f299bf3d7ca7edd2bc1
ShymkoGleb <71500261+ShymkoGleb@users.noreply.github.com>
2021-01-21 03:57:57 +0200

    Adding timer and intent beetween  main and score activity

--- app/src/main/java/com/example/shymko_firstproject/Activity/ScoreScreenActivity.kt
+++ app/src/main/java/com/example/shymko_firstproject/Activity/ScoreScreenActivity.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.widget.Toast
 import com.example.shymko_firstproject.R
+import kotlinx.android.synthetic.main.activity_score_screen.*
 
 class ScoreScreenActivity : AppCompatActivity() {
 
     companion object {
         fun start(context: Context) {
-              val intent = Intent(context, ScoreScreenActivity::class.java)
+            val intent = Intent(context, ScoreScreenActivity::class.java)
             context.startActivity(intent)
         }
     }
@@ -18,25 +22,35 @@ class ScoreScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_score_screen)
-       // timeCalculation()
 
+        val firstTeam: String? = intent.getStringExtra("firstTeam")
+        val secondTeam: String? = intent.getStringExtra("secondTeam")
+        timeCalculation()
+    }
 
+    override fun onStart() {
+        super.onStart()
+        fun timeCalculation() {
+
+        }
     }
 
 
-    /*fun timeCalculation() {
+    fun timeCalculation() {
 
-        var timeOfHid: Long = ((tvCounter + 1) / 10) * 1000
+        var timeOfHid: Long = ((100 + 1) / 10) * 1000
         object : CountDownTimer(timeOfHid, 1000) {
             override fun onTick(millisUntilFinished: Long) {
+                Log.d("onTick", "hello on tick")
+                tvCountdownTimer.text = "Timer: " + millisUntilFinished / 1000
             }
 
             override fun onFinish() {
-                startTriger = false
-                Log.d("onFinish", "startTriger $startTriger")
+                //Toast.makeText(, "TIME IS UP", Toast.LENGTH_SHORT).show()
+                Log.d("onFinish", "TIME IS UP")
                 onStart()
             }
         }.start()
 
-    }*/
+    }
 }
